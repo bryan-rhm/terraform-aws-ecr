a71943c25f0d8a96b3ea2541e509df02c854973d
Bryan Recinos <bryan.rhm1@gmail.com>
2022-06-18 16:12:28 -0600

    [v1.0.0] added default tags to resource

--- main.tf
+++ main.tf
@@ -10,6 +10,8 @@ resource "aws_ecr_repository" "this" {
   image_scanning_configuration {
     scan_on_push = var.scan_on_push
   }
+
+  tags = var.tags
 }
 
 resource "aws_ecr_lifecycle_policy" "this" {
